0a503f67437db5b7a2eded487f7b883ea11c4ea1
jmc <jmc>
2001-02-20 14:48:07 +0000

    remove unused CPP_kees
    Former-commit-id: 160f3f418604a5cabf158c62f6371981adb7d774

--- verification/aim.5l_zon-ave/code/CPP_OPTIONS.h
+++ verification/aim.5l_zon-ave/code/CPP_OPTIONS.h
@@ -16,6 +16,11 @@ C o Include/exclude code for GM/Redi parameterization
 C o Include/exclude code for KPP mixing scheme
 #undef  ALLOW_KPP
 
+C o Shortwave heating as extra term in external_forcing.F
+#ifdef ALLOW_KPP
+#define  SHORTWAVE_HEATING
+#endif
+
 C o Include/exclude code for Shapiro filters
 #define ALLOW_SHAP_FILT
 
@@ -100,14 +105,8 @@ C o Include/exclude momentum eqn metric terms code
 C o Include/exclude phi_hyd calculation code
 #define INCLUDE_PHIHYD_CALCULATION_CODE
 
-C o Include/exclude call to S/R FIND_RHO
-#define INCLUDE_FIND_RHO_CALL
-
 C o Include/exclude call to S/R CONVECT
 #define INCLUDE_CONVECT_CALL
-
-C o Include/exclude call to S/R CALC_ISOSLOPES
-#define INCLUDE_CALC_ISOSLOPES_CALL
 
 C o Include/exclude call to S/R CALC_DIFFUSIVITY
 #define INCLUDE_CALC_DIFFUSIVITY_CALL
@@ -137,6 +136,4 @@ C o Include/exclude code specific to the ECCO/SEALION version.
 #ifdef INCLUDE_ECCO_PACKAGE
 #include "ECCO_CPPOPTIONS.h"
 #endif
-
-
 
